1b572463609595bf86ba7df1c9bec3065c4b94e6
AmerigoPassaro <110347819+AmerigoPassaro@users.noreply.github.com>
2024-03-11 18:13:58 +0100

    Inizializzazione
    Caricamento progetto

---
+++ assets/js/base.js
@@ -0,0 +1,11 @@
+let header = document.createElement('header');
+document.body.prepend(header);
+
+let nav = document.createElement('nav');
+document.body.append(nav);
+
+let main = document.createElement('main');
+document.body.append(main);
+
+let footer = document.createElement('footer');
+document.body.append(footer);
