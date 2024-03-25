79f9ebbbde579558c74f49acbb0f6a65dbf1aefc
AmerigoPassaro <110347819+AmerigoPassaro@users.noreply.github.com>
2024-03-25 18:15:32 +0100

    Modifica
    Caricamento Base.JS, Menu.JS
    
    Caricamento Index.HTML

--- assets/js/base.js
+++ assets/js/base.js
@@ -3,10 +3,10 @@ parts.forEach(
     function part(elem,ind,arr){
         // Creazione Label
             var elem = document.createElement(`${elem}`);
+            eval(elem).setAttribute('id', `${arr[ind]}-section`);
         // Inserimento Label nella pagina
             document.body.append(elem);
     }   
 );
-
 
 let controllo = true;
--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,41 +1,29 @@
-let elements = ['gotoHead()', 'gotoSearch()', 'gotExplenation()'];
-
-
-
+let elements = ['header', 'search', 'explain'];
 elements.forEach(
     function elements(elem,ind,arr){
         // Creazione Voce menu
             var elem = document.createElement('button');
+        // Creazione Funzione menu
+            var jump = function (){
+                document.querySelector(`#${arr[ind]}-section`).scrollIntoView(top=true);
+                for(let i = 0; i < arr.length; i++){
+                    document.querySelectorAll('.menu-item')[i].style.width = "10px";
+                    document.querySelectorAll('.menu-item')[i].style.height = "10px";
+                }
+                document.querySelectorAll('.menu-item')[ind].style.width = "17px";
+                document.querySelectorAll('.menu-item')[ind].style.height = "17px";
+            };
         // Inserimento attributi Voce menu
             eval(elem).setAttribute('id', `menuI${ind}`);
             eval(elem).setAttribute('class', 'menu-item');
-            eval(elem).setAttribute('onclick', `${arr[ind]}`);
         // Inserimento Voce menu nella pagina
             document.body.querySelector('nav').append(eval(elem));
+            document.querySelector(`#menuI${ind}`).addEventListener('click', jump);
     }   
 );
 
 document.addEventListener('DOMContentLoaded', function(){
-    header.scrollIntoView(top=true);
+    document.querySelector('header').scrollIntoView(top=true);
     document.querySelectorAll('.menu-item')[0].style.width = "17px";
     document.querySelectorAll('.menu-item')[0].style.height = "17px";
 });
-
-function gotoHead(){
-    header.scrollIntoView(top=true);
-    document.querySelectorAll('.menu-item')[0].style.width = "17px";
-    document.querySelectorAll('.menu-item')[0].style.height = "17px";
-    document.querySelectorAll('.menu-item')[1].style.width = "10px";
-    document.querySelectorAll('.menu-item')[1].style.height = "10px";
-}
-
-function gotoSearch(){
-    searchSection.scrollIntoView(top=true);
-
-    if(searchSection.getBoundingClientRect().bottom=true){
-        document.querySelectorAll('.menu-item')[1].style.width = "17px";
-        document.querySelectorAll('.menu-item')[1].style.height = "17px";
-        document.querySelectorAll('.menu-item')[0].style.width = "10px";
-        document.querySelectorAll('.menu-item')[0].style.height = "10px";
-    }
-}
