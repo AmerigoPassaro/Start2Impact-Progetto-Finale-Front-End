d403964533a559a17e76d6a5f7e91595a3d516ed
Amerigo Passaro <110347819+AmerigoPassaro@users.noreply.github.com>
2024-06-07 12:05:35 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react"
-import Meditation from "./meditation/Meditation"
-
+import * as React from "react"
+import Emissions from "./emissions/Emissions"
 
 export default function App(){
+  document.body.style.overflow = "hidden"
   return(
     <>
-    <Meditation />
+    <Emissions />
     </>
   )
 }
