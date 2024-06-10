daceb7449daebe5d555bf6dc563b241465efe513
AmerigoPassaro <110347819+AmerigoPassaro@users.noreply.github.com>
2024-06-10 11:52:03 +0200

    Correzione errore
    Ho trovato difficoltà nella navigazione in quanto non è possibile scrollare la pagina per raggiungere il form.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import * as React from "react"
 import Emissions from "./emissions/Emissions"
 
 export default function App(){
-  document.body.style.overflow = "hidden"
   return(
     <>
     <Emissions />
